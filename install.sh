e6da28085f465a15b9beba7dca5d41e50c88425e
lemonadeforlife <nahianlabiblimon44@gmail.com>
2022-09-27 17:57:46 +0600

    added install.sh for linux installation

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 cd ~/.local/share/applications/
-wget -c "https://github.com/lemonadeforlife/gobulk/releases/download/latest/gobulk.tar.gz" | tar -xz
+mkdir gobulk && cd gobulk
+wget --content-disposition https://github.com/lemonadeforlife/gobulk/releases/download/latest/gobulk.tar.gz
+tar -zxf gobulk.tar.gz
+rm -rf gobulk.tar.gz
 python3 -m pip install -r requirements.txt
-cd gobulk && ln -s downime.py gobulk
-mv gobulk ~/.local/bin
-clear
+ln -s ~/.local/share/applications/gobulk/downime.py ~/.local/bin/gobulk
 echo Installation complete!
-gobulk login
+echo
+echo run gobulk login
