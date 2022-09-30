9f85d8f8cae778d64cbaf5ad15c847dff48b7716
lemonadeforlife <nahianlabiblimon44@gmail.com>
2022-09-30 15:38:09 +0600

    rewrite install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 cd ~/.local/share/applications/
-mkdir gobulk && cd gobulk
-wget --content-disposition https://github.com/lemonadeforlife/gobulk/releases/download/latest/gobulk.tar.gz
-tar -zxf gobulk.tar.gz
-rm -rf gobulk.tar.gz
+git clone https://github.com/lemonadeforlife/gobulk.git
+cd gobulk
 python3 -m pip install -r requirements.txt
 ln -s ~/.local/share/applications/gobulk/downime.py ~/.local/bin/gobulk
 echo Installation complete!
